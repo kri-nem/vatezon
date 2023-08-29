390723d35c89a7dfe7a902eff03e506bd8306e5b
Kristóf Németh <nemethkristof1@gmail.com>
2023-08-29 11:49:09 +0200

    add access modifier

--- auction/src/main/java/com/codecool/auction/controller/ProductController.java
+++ auction/src/main/java/com/codecool/auction/controller/ProductController.java
@@ -1,11 +1,12 @@
 package com.codecool.auction.controller;
 
 import com.codecool.auction.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    ProductService productService;
+    private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
