f49020d68876b7760d17f7acca327b33df3ea8f6
fulopco <you@example.com>
2023-08-29 13:45:02 +0200

    create endpoint to get all products for grid view in JSON

--- auction/src/main/java/com/codecool/auction/controller/ProductController.java
+++ auction/src/main/java/com/codecool/auction/controller/ProductController.java
@@ -1,14 +1,28 @@
 package com.codecool.auction.controller;
 
+import com.codecool.auction.controller.dto.ProductGridViewDTO;
 import com.codecool.auction.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.List;
+
 @RestController
+@RequestMapping("api/products")
 public class ProductController {
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
+    @GetMapping("/all")
+    public Collection<ProductGridViewDTO> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    
 }
--- auction/src/main/java/com/codecool/auction/service/ProductService.java
+++ auction/src/main/java/com/codecool/auction/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.codecool.auction.service;
 
+import com.codecool.auction.controller.dto.ProductGridViewDTO;
 import com.codecool.auction.service.model.Product;
 import com.codecool.auction.service.model.ProductType;
 import com.codecool.auction.service.model.User;
@@ -7,13 +8,13 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.net.URL;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
     private static int nextId = 1;
-    private Collection<Product> products;
+    private final Collection<Product> products;
 
     public ProductService() {
         this.products = new HashSet<>();
@@ -24,5 +25,13 @@ public class ProductService {
         Product product = new Product(nextId++, name, description, price, pictures, uploader, productType);
         products.add(product);
         return product;
+    }
+
+    public Set<ProductGridViewDTO> getAllProducts () {
+        return products.stream().map(product -> convertProductToGridViewDTO(product)).collect(Collectors.toSet());
+    }
+
+    private ProductGridViewDTO convertProductToGridViewDTO (Product product) {
+        return new ProductGridViewDTO(product.getName(), product.getPrice(), product.getFirstPictures());
     }
 }
