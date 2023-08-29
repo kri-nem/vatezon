ffc0d8498fb3feb9a68d88ab3d2b11c45c05d714
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-08-29 13:19:06 +0200

    implement a PostMapping for adding new product

--- auction/src/main/java/com/codecool/auction/controller/ProductController.java
+++ auction/src/main/java/com/codecool/auction/controller/ProductController.java
@@ -1,14 +1,25 @@
 package com.codecool.auction.controller;
 
+import com.codecool.auction.controller.dto.NewProductDTO;
 import com.codecool.auction.service.ProductService;
+import com.codecool.auction.service.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/products")
 public class ProductController {
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @PostMapping("new")
+    public Product addNewProduct (@RequestBody NewProductDTO newProduct) {
+       return productService.addNewProduct(newProduct);
     }
 }
