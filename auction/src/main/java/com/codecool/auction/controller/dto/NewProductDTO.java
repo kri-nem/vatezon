d9aacae2d818f47fbadbbd93b2df4ff9b3e50277
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-08-29 13:12:05 +0200

    add a new dto record for handling incoming new product

--- auction/src/main/java/com/codecool/auction/controller/dto/NewProductDTO.java
+++ auction/src/main/java/com/codecool/auction/controller/dto/NewProductDTO.java
@@ -1,4 +1,11 @@
 package com.codecool.auction.controller.dto;
 
-public class NewProductDTO {
-}
+import com.codecool.auction.service.model.ProductType;
+import com.codecool.auction.service.model.User;
+
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.Collection;
+
+public record NewProductDTO (String name, String description, BigDecimal price, Collection<URL> pictures,
+                             User uploader, ProductType productType) {}
