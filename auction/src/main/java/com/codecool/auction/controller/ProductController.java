f3c3c787cbe04651f64a248e77db03fd031493ff
Kristóf Németh <nemethkristof1@gmail.com>
2023-08-29 14:40:31 +0200

    add request mapping to products controller

--- auction/src/main/java/com/codecool/auction/controller/ProductController.java
+++ auction/src/main/java/com/codecool/auction/controller/ProductController.java
@@ -1,10 +1,11 @@
 package com.codecool.auction.controller;
 
 import com.codecool.auction.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/products")
 public class ProductController {
     private final ProductService productService;
 
