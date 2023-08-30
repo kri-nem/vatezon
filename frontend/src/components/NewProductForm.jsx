9fbd28c8850b473c5088250d3d98453433b5013b
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-08-30 11:41:29 +0200

    change product form to controlled

--- frontend/src/components/NewProductForm.jsx
+++ frontend/src/components/NewProductForm.jsx
@@ -1,47 +1,54 @@
+import { useState } from "react";
 
 const NewProductForm = ({productTypes, addNewProduct}) => {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [productType, setProductType] = useState("");
+    const [pictureURL, setPictureURL] = useState("");
+
+    
     const onSubmit = (e) => {
         e.preventDefault();
-        const formData = new FormData(e.target);
-        const entries = [...formData.entries()];
-    
-        const product = entries.reduce((acc, entry) => {
-          const [k, v] = entry;
-          acc[k] = v;
-          return acc;
-        }, {});
-    
+        const product = {
+            name: name,
+            description: description,
+            price: price,
+            productType: productType,
+            pictureURL: pictureURL,
+        }
         console.log(product);
         return addNewProduct(product);
-      };
+    }
 
     return (
         <form className="NewProductForm" onSubmit={onSubmit}>
             <div className="control">
                 <label htmlFor="name">Name:</label>
-                <input name="name" id="name"/>
+                <input name="name" id="name" defaultValue={name} onChange={(e) => setName(e.target.value)}/>
             </div>
 
             <div className="control">
                 <label htmlFor="description">Description:</label>
-                <input name="description" id="description"/>
+                <input name="description" id="description" defaultValue={description} onChange={(e) => setDescription(e.target.value)}/>
             </div>
 
             <div className="control">
                 <label htmlFor="price">Price:</label>
-                <input name="price" id="price"/>
-            </div>
-
-            <div className="control">
-                <label htmlFor="username">Username:</label>
-                <input name="username" id="username"/>
+                <input name="price" id="price" defaultValue={price} onChange={(e) => setPrice(e.target.value)}/>
             </div>
 
             <div>
                 <label htmlFor="productType">Product type:</label>
-                <select>
-                    {productTypes.map((o,i) => <option key={i}>{o}</option>)}
+                <select onChange={(e) => setProductType(e.target.value)}>
+                    <option disabled selected>Select a type!</option>
+                    {productTypes.map((o,i) => <option key={i} value={o}>{o}</option>)}
                 </select>
+            </div>
+
+            <div className="control">
+                <label htmlFor="pictureURL">Picture URL:</label>
+                <input name="pictureURL" id="pictureURL" defaultValue={pictureURL} onChange={(e) => setPictureURL(e.target.value)}/>
             </div>
 
             <div className="button">
