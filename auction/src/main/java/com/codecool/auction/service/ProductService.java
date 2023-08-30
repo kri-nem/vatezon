c9a4c41196c50191cc4ab2a59673ae63669b2297
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-08-30 12:08:43 +0200

    add a static USER instance to class

--- auction/src/main/java/com/codecool/auction/service/ProductService.java
+++ auction/src/main/java/com/codecool/auction/service/ProductService.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
 @Service
 public class ProductService {
+    private static final User THE_USER = new User("user1");
     private static int nextId = 1;
     private Collection<Product> products;
 
@@ -20,9 +22,9 @@ public class ProductService {
         this.products = new HashSet<>();
     }
 
-    public Product addNewProduct(String name, String description, BigDecimal price, Collection<URL> pictures,
+    public Product addNewProduct(String name, String description, BigDecimal price, String picture,
                                  User uploader, ProductType productType) {
-        Product product = new Product(nextId++, name, description, price, pictures, uploader, productType);
+        Product product = new Product(nextId++, name, description, price, picture, uploader, productType);
         products.add(product);
         return product;
     }
@@ -30,10 +32,11 @@ public class ProductService {
     public Product addNewProduct(NewProductDTO newProduct){
         String name = newProduct.name();
         String description = newProduct.description();
-        BigDecimal price = newProduct.price();
-        Collection<URL> pictures = newProduct.pictures();
-        User uploader = newProduct.uploader();
-        ProductType productType = newProduct.productType();
-        return addNewProduct(name, description, price, pictures, uploader, productType);
+        String price = newProduct.price();
+        BigDecimal product_price = new BigDecimal(price);
+        String pictureURL = newProduct.pictureURL();
+        String productType = newProduct.productType();
+        ProductType product_type = Arrays.stream(ProductType.values()).filter(e -> e.hasSameText(productType)).findFirst().orElse(null);
+        return addNewProduct(name, description, product_price, pictureURL, THE_USER, product_type);
     }
 }
