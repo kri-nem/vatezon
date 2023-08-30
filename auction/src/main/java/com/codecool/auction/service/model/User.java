7daefc0fe97b682f75cbe8914d86c88005647400
Kristóf Németh <nemethkristof1@gmail.com>
2023-08-30 13:02:20 +0200

    add constructor to user

--- auction/src/main/java/com/codecool/auction/service/model/User.java
+++ auction/src/main/java/com/codecool/auction/service/model/User.java
@@ -3,6 +3,10 @@ package com.codecool.auction.service.model;
 public class User {
     private String userName;
 
+    public User(String userName) {
+        this.userName = userName;
+    }
+
     public String getUserName() {
         return userName;
     }
