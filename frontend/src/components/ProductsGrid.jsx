30f1cd6c18f5f7d263725e6eb5cb7f81354306ea
fulopco <you@example.com>
2023-08-31 10:14:48 +0200

    display products based on filtered search

--- frontend/src/components/ProductsGrid.jsx
+++ frontend/src/components/ProductsGrid.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react"
 
-export default function ProductsGrid({products}) {
+export default function ProductList({ products, onNameFilterChange }) {
+
     return (
         <div>
-            {products.map((product) => (
-                <>
-                    <h4>{product.name}</h4>
-                    <h4>{product.price}</h4>
-                    <h4>{product.pictureURL}</h4>
-                </>
-            ))}
+            <input name="filter" onChange={(e) => onNameFilterChange(e.target.value)} />
+            <div>
+                {products.map((product) => (
+                    <>
+                        <h4>{product.name}</h4>
+                        <h4>{product.price}</h4>
+                        <h4>{product.pictureURL}</h4>
+                    </>
+                ))}
+            </div>
         </div>
     )
 }
