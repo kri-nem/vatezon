1afcf4b48c4b5a7bd3464d6dbf8eef37f2de51dd
fulopco <you@example.com>
2023-08-31 19:07:34 +0200

    make search visible

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -3,16 +3,13 @@ import ProductList from "../components/ProductsList"
 
 const Products = () => {
     const [products, setProducts] = useState([])
-    const [filter, setFilter] = useState()
-    //const [name, setName] = useState('') miert nem up-to-date a set-em? async tudom de hogy lehetne megoldani
+    const [filter, setFilter] = useState('name/')
 
     useEffect(() => {
         fetch(`/api/products/`)
         .then(res => res.json())
         .then((data) => {
             setProducts(data)
-            console.log(data);
-            //console.log(products);
         })
         .catch(err => console.error(err))
     }, [])
@@ -23,7 +20,6 @@ const Products = () => {
     }
 
     const chooseCategory = (e) => {
-        //setName(e.target.value)
         if (filter !== '' && e.target.value !== '') {
             let path = `api/products/${filter + e.target.value}`
             fetch(path)
@@ -38,8 +34,6 @@ const Products = () => {
             .then(res => res.json())
             .then((data) => {
                 setProducts(data)
-                console.log(data);
-                //console.log(products);
             })
             .catch(err => console.error(err))
         }
