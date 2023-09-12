c894befd2178b0924889977681eab8a957556f59
Kristóf Németh <nemethkristof1@gmail.com>
2023-09-12 10:10:10 +0200

    make product service use product DAO, delete previous implementations

--- auction/src/main/java/com/codecool/auction/service/ProductService.java
+++ auction/src/main/java/com/codecool/auction/service/ProductService.java
@@ -3,83 +3,88 @@ package com.codecool.auction.service;
 import com.codecool.auction.controller.dto.NewProductDTO;
 import com.codecool.auction.controller.dto.ProductDetailedViewDTO;
 import com.codecool.auction.controller.dto.ProductGridViewDTO;
-import com.codecool.auction.service.model.Product;
-import com.codecool.auction.service.model.ProductType;
-import com.codecool.auction.service.model.User;
+import com.codecool.auction.repository.ProductDAO;
+import com.codecool.auction.model.Product;
+import com.codecool.auction.model.ProductType;
+import com.codecool.auction.model.User;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductService {
-  private static final User THE_USER = new User("user1");
-  private static int nextId = 1;
-  private final List<Product> products;
+  //static final User THE_USER = new User("user1");
+  final ProductDAO productDAO;
 
-  public ProductService() {
-    this.products = new ArrayList<>();
+  @Autowired
+  public ProductService(ProductDAO productDAO) {
+    this.productDAO = productDAO;
   }
 
   public List<ProductGridViewDTO> getAllProducts() {
-    return products.stream().map(this::convertProductToGridViewDTO).toList();
+    return null; /*products.stream().map(this::convertProductToGridViewDTO).toList();*/
   }
 
   public List<ProductGridViewDTO> getProductsByName(String name) {
-    return products.stream().filter(product -> product.getName().contains(name))
-            .map(this::convertProductToGridViewDTO).toList();
+    return null; /*products.stream().filter(product -> product.getName().contains(name))
+            .map(this::convertProductToGridViewDTO).toList();*/
   }
 
   public List<ProductGridViewDTO> getProductsByCategory (String category) {
-    return products.stream()
+    return null; /*products.stream()
             .filter(product -> product.getProductType().equals(ProductType.valueOf(category)))
             .map(this::convertProductToGridViewDTO)
-            .toList();
+            .toList();*/
   }
 
   public Product addNewProduct(String name, String description, BigDecimal price, String pictureURL,
                                User uploader, ProductType productType) {
-    Product product = new Product(nextId++, name, description, price, pictureURL, uploader, productType);
+    /*Product product = new Product(nextId++, name, description, price, pictureURL, uploader, productType);
     products.add(product);
-    return product;
+    return product;*/
+    return null;
   }
 
   //just for testing
   public Product addProduct(ProductGridViewDTO productDTO) {
-    Product p = convertDTOToProducts(productDTO);
+    /*Product p = convertDTOToProducts(productDTO);
     products.add(p);
-    return p;
+    return p;*/
+    return null;
   }
 
   public Product addNewProduct(NewProductDTO newProduct) {
-    String name = newProduct.name();
+    /*String name = newProduct.name();
     String description = newProduct.description();
     String price = newProduct.price();
     BigDecimal product_price = new BigDecimal(price);
     String pictureURL = newProduct.pictureURL();
     String productType = newProduct.productType();
     ProductType product_type = Arrays.stream(ProductType.values()).filter(e -> e.hasSameText(productType)).findFirst().orElse(null);
-    return addNewProduct(name, description, product_price, pictureURL, THE_USER, product_type);
+    return addNewProduct(name, description, product_price, pictureURL, THE_USER, product_type);*/
+    return null;
   }
 
   //just for testing
   private Product convertDTOToProducts(ProductGridViewDTO productDTO) {
-    return new Product(nextId, productDTO.name(), "null-jet", productDTO.price(), productDTO.pictureURL(), null, productDTO.type());
+    return null; /*new Product(nextId, productDTO.name(), "null-jet", productDTO.price(), productDTO.pictureURL(), null, productDTO.type());*/
   }
 
   private ProductGridViewDTO convertProductToGridViewDTO(Product product) {
-    return new ProductGridViewDTO(product.getName(), product.getPrice(), product.getPictureURL(), product.getId(), product.getProductType());
+    return null; /*new ProductGridViewDTO(product.getName(), product.getPrice(), product.getPictureURL(), product.getId(), product.getProductType());*/
   }
 
     public ProductDetailedViewDTO getProductDetailedViewDTO(String id) {
-        Optional<Product> product = products.stream()
+        /*Optional<Product> product = products.stream()
                 .filter(p -> p.hasId(id)).findFirst();
         if (product.isPresent()) {
             return product.map(ProductDetailedViewDTO::new).get();
-        }
+        }*/
         return null;
     }
 }
