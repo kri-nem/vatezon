172bf4a185fbb92793a9cce48da986ee7402d03f
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-09-12 13:39:01 +0200

    add a method for getting a user by username

--- auction/src/main/java/com/codecool/auction/repository/UserDAO.java
+++ auction/src/main/java/com/codecool/auction/repository/UserDAO.java
@@ -4,4 +4,5 @@ import com.codecool.auction.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDAO extends JpaRepository<User, Long> {
+    User getUserByUserName (String username);
 }
