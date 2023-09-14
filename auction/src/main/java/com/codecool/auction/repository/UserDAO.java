7e8b43e7ec6ed92c3e9b2bda3918ff0753f480bd
Kristóf Németh <nemethkristof1@gmail.com>
2023-09-14 11:21:57 +0200

    add request for user by is

--- auction/src/main/java/com/codecool/auction/repository/UserDAO.java
+++ auction/src/main/java/com/codecool/auction/repository/UserDAO.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDAO extends JpaRepository<User, Long> {
     User getUserByUserName (String username);
+    User getUserById(Long id);
 }
