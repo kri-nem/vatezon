34a43eab1501fdf0d27e766620984fd789700ac7
fulopco <you@example.com>
2023-09-14 11:06:31 +0200

    replace ProductsType enum to Tag class and updated the relevant classes

--- auction/src/main/java/com/codecool/auction/controller/dto/NewProductDTO.java
+++ auction/src/main/java/com/codecool/auction/controller/dto/NewProductDTO.java
@@ -1,7 +1,14 @@
 package com.codecool.auction.controller.dto;
 
+import com.codecool.auction.model.Tag;
+import com.codecool.auction.model.User;
+
+import java.util.Set;
+
 public record NewProductDTO (String name,
                              String description,
                              String price,
-                             String pictureURL,
-                             String productType) {}
+                             String picture,
+                             User uploader,
+                             User buyer,
+                             Set<String> tags) {}
--- auction/src/main/java/com/codecool/auction/controller/dto/ProductDetailedViewDTO.java
+++ auction/src/main/java/com/codecool/auction/controller/dto/ProductDetailedViewDTO.java
@@ -1,9 +1,12 @@
 package com.codecool.auction.controller.dto;
 
 import com.codecool.auction.model.Product;
+import com.codecool.auction.model.Tag;
+
+import java.util.Set;
 
 public record ProductDetailedViewDTO(String name, String description, String price, String pictureUrl,
-                                     String productType, String uploader, String buyer) {
+                                     Set<Tag> tags, String uploader, String buyer) {
 
     public ProductDetailedViewDTO(Product product) {
         this(
@@ -11,7 +14,7 @@ public record ProductDetailedViewDTO(String name, String description, String pri
         product.getDescription(),
         product.getPrice().toString(),
         product.getPicture(),
-        product.getProductType().getText(),
+        product.getTags(),
         product.getUploader().getUserName(),
         product.getBuyer() == null ? "" : product.getBuyer().getUserName());
     }
