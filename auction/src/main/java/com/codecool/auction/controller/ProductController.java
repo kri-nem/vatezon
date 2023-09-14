0be37b6d449c47f7ff4564ad4f6a4714e811d9cd
fulopco <you@example.com>
2023-09-14 11:11:35 +0200

    update and connect all Product Beans with Tag class

--- auction/src/main/java/com/codecool/auction/controller/ProductController.java
+++ auction/src/main/java/com/codecool/auction/controller/ProductController.java
@@ -3,23 +3,30 @@ package com.codecool.auction.controller;
 import com.codecool.auction.controller.dto.NewProductDTO;
 import com.codecool.auction.controller.dto.ProductDetailedViewDTO;
 import com.codecool.auction.controller.dto.ProductGridViewDTO;
+import com.codecool.auction.model.Tag;
 import com.codecool.auction.service.ProductService;
 import com.codecool.auction.model.Product;
-import com.codecool.auction.model.ProductType;
+import com.codecool.auction.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api/products")
 public class ProductController {
-    private final ProductService productService;
 
-    public ProductController(@Autowired ProductService productService) { this.productService = productService;
+    private final ProductService productService;
+    private final TagService tagService;
+
+    @Autowired
+    public ProductController(ProductService productService, TagService tagService) {
+        this.productService = productService;
+        this.tagService = tagService;
     }
+
 
     @GetMapping("detailed/{id}")
     public ProductDetailedViewDTO getDetailedView(@PathVariable("id") String id) {
@@ -31,30 +38,20 @@ public class ProductController {
         return productService.addNewProduct(newProduct);
     }
 
-    @GetMapping("types")
-    public List<String> getProductTypes () {
-        return Arrays.stream(ProductType.values()).map(ProductType::getText).toList();
-    }
-
     @GetMapping("/")
     public List<ProductGridViewDTO> getAllProducts() {
         return productService.getAllProducts();
     }
 
     @GetMapping("/category/{category}")
-    public List<ProductGridViewDTO> categorizeProductsBasedOnFilter (@PathVariable String category) {
-        return productService.getProductsByCategory(category.toUpperCase());
+    public Set<ProductGridViewDTO> categorizeProductsBasedOnFilter (@PathVariable String category) {
+        Tag tag = tagService.findTagByEndpoint(category);
+        return productService.getProductsByTag(tag);
     }
 
     @GetMapping("/name/{name}")
     public Collection<ProductGridViewDTO> getProductsByName (@PathVariable String name) {
         return productService.getProductsByName(name);
-    }
-
-    //just for postman testing
-    @PostMapping("/add")
-    public Product addProducts (@RequestBody ProductGridViewDTO product) {
-        return productService.addProduct(product);
     }
 
 }
--- auction/src/main/java/com/codecool/auction/repository/ProductDAO.java
+++ auction/src/main/java/com/codecool/auction/repository/ProductDAO.java
@@ -2,7 +2,20 @@ package com.codecool.auction.repository;
 
 
 import com.codecool.auction.model.Product;
+import com.codecool.auction.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
+@Repository
 public interface ProductDAO extends JpaRepository<Product, Long> {
+
+  @Override
+  List<Product> findAll();
+
+  List<Product> findProductByNameContaining(String name);
+
+  Set<Product> findProductByTagsContaining(Tag name);
 }
