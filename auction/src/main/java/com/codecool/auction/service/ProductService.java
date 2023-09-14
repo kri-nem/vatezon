60f776af4d7e9efb77747f7206f291d35ee2c2c6
Kristóf Németh <nemethkristof1@gmail.com>
2023-09-14 14:25:27 +0200

    add new product with image saved to file, data saved to DB

--- auction/src/main/java/com/codecool/auction/service/ProductService.java
+++ auction/src/main/java/com/codecool/auction/service/ProductService.java
@@ -5,7 +5,6 @@ import com.codecool.auction.controller.dto.ProductDetailedViewDTO;
 import com.codecool.auction.controller.dto.ProductGridViewDTO;
 import com.codecool.auction.repository.ProductDAO;
 import com.codecool.auction.model.Product;
-import com.codecool.auction.model.ProductType;
 import com.codecool.auction.model.User;
 import com.codecool.auction.repository.UserDAO;
 import com.codecool.auction.model.Tag;
@@ -14,6 +13,9 @@ import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,50 +26,80 @@ import java.util.stream.Collectors;
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductService {
-  static final Path PICTURES_PATH = Paths.get("uploaded_pictures/").toAbsolutePath();
-  final ProductDAO productDAO;
-  final UserDAO userDao;
-  final TagDAO tagDAO;
+    static final Path PICTURES_PATH = Paths.get("uploaded_pictures/").toAbsolutePath();
+    final ProductDAO productDAO;
+    final UserDAO userDao;
+    final TagDAO tagDAO;
 
-  @Autowired
-  public ProductService(ProductDAO productDAO, UserDAO userDao, TagDAO tagDAO) {
-    this.productDAO = productDAO;
-    this.userDao = userDao;
-    this.tagDAO = tagDAO;
-  }
+    @Autowired
+    public ProductService(ProductDAO productDAO, UserDAO userDao, TagDAO tagDAO) {
+        this.productDAO = productDAO;
+        this.userDao = userDao;
+        this.tagDAO = tagDAO;
+    }
 
-  public List<ProductGridViewDTO> getAllProducts() {
-    return productDAO.findAll().stream().map(this::convertProductToGridViewDTO).toList();
-  }
+    public List<ProductGridViewDTO> getAllProducts() {
+        return productDAO.findAll().stream()
+                .map(this::convertProductToGridViewDTO)
+                .toList();
+    }
 
-  public List<ProductGridViewDTO> getProductsByName(String name) {
-    return productDAO.findProductByNameContaining(name).stream().map(this::convertProductToGridViewDTO).toList();
-  }
+    public List<ProductGridViewDTO> getProductsByName(String name) {
+        return productDAO.findProductByNameContaining(name).stream()
+                .map(this::convertProductToGridViewDTO)
+                .toList();
+    }
 
-  public Set<ProductGridViewDTO> getProductsByTag(Tag tag) {
-    return productDAO.findProductByTagsContaining(tag).stream().map(this::convertProductToGridViewDTO).collect(Collectors.toSet());
-  }
-  public Product addNewProduct(Long userId, NewProductDTO newProduct) {
-    User uploader = userDao.getUserById(userId);
-    Product product = Product.builder()
-            .name(newProduct.name())
-            .description(newProduct.description())
-            .price(new BigDecimal(newProduct.price()))
-            .uploader(userDao.getUserById(userId))
-            .build();
+    public Set<ProductGridViewDTO> getProductsByTag(Tag tag) {
+        return productDAO.findProductByTagsContaining(tag).stream()
+                .map(this::convertProductToGridViewDTO)
+                .collect(Collectors.toSet());
+    }
 
-    productDAO.save(product);
+    public Product addNewProduct(Long userId, NewProductDTO newProduct) throws IOException {
+        User uploader = userDao.getUserById(userId);
+        Set<Tag> tags = newProduct.tags().stream()
+                .map(tagDAO::findByName)
+                .collect(Collectors.toSet());
 
-    Long pId = product.getId();
-    return product;
+        Product product = Product.builder()
+                .name(newProduct.name())
+                .description(newProduct.description())
+                .price(new BigDecimal(newProduct.price()))
+                .uploader(userDao.getUserById(userId))
+                .tags(tags)
+                .build();
 
-  }
+        String contentType = newProduct.picture().getContentType();
+        String extension = "";
+        if (contentType != null) {
+            extension = contentType.split("/")[contentType.split("/").length - 1];
+        }
 
-  private ProductGridViewDTO convertProductToGridViewDTO(Product product) {
-    return new ProductGridViewDTO(product.getName(), product.getPrice(), product.getPicture(), product.getId(), product.getTags());
-  }
+        String fileName = userId.toString() + "-" + product.getId() + "." + extension;
+        File destination = new File(PICTURES_PATH + "/" + fileName);
+
+        newProduct.picture().transferTo(destination);
+
+        product.setPicture(fileName);
+
+        productDAO.save(product);
+
+        return product;
+
+    }
+
+    private ProductGridViewDTO convertProductToGridViewDTO(Product product) {
+        return new ProductGridViewDTO(
+                product.getName(),
+                product.getPrice(),
+                product.getPicture(),
+                product.getId(),
+                product.getTags());
+    }
 
     public ProductDetailedViewDTO getProductDetailedViewDTO(String id) {
+        //TODO
         /*Optional<Product> product = products.stream()
                 .filter(p -> p.hasId(id)).findFirst();
         if (product.isPresent()) {
