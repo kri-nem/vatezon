99b8bd9c53e2c655f2dfedfa2b3ff520ea3ce1a6
Kristóf Németh <nemethkristof1@gmail.com>
2023-09-14 14:09:44 +0200

    remove unused methods

--- auction/src/main/java/com/codecool/auction/controller/ProductController.java
+++ auction/src/main/java/com/codecool/auction/controller/ProductController.java
@@ -3,15 +3,15 @@ package com.codecool.auction.controller;
 import com.codecool.auction.controller.dto.NewProductDTO;
 import com.codecool.auction.controller.dto.ProductDetailedViewDTO;
 import com.codecool.auction.controller.dto.ProductGridViewDTO;
+import com.codecool.auction.model.Product;
 import com.codecool.auction.model.Tag;
 import com.codecool.auction.service.ProductService;
-import com.codecool.auction.model.Product;
 import com.codecool.auction.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -42,13 +42,8 @@ public class ProductController {
             method = POST, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     public @ResponseBody Product addNewProduct (
             @PathVariable("user-id") Long userId,
-            @ModelAttribute NewProductDTO newProduct) {
+            @ModelAttribute NewProductDTO newProduct) throws IOException {
         return productService.addNewProduct(userId, newProduct);
-    }
-
-    @GetMapping("types")
-    public List<String> getProductTypes () {
-        return Arrays.stream(ProductType.values()).map(ProductType::getText).toList();
     }
 
     @GetMapping("/")
@@ -65,12 +60,6 @@ public class ProductController {
     @GetMapping("/name/{name}")
     public Collection<ProductGridViewDTO> getProductsByName (@PathVariable String name) {
         return productService.getProductsByName(name);
-    }
-
-    //just for postman testing
-    @PostMapping("/add")
-    public Product addProducts (@RequestBody ProductGridViewDTO product) {
-        return productService.addProduct(product);
     }
 
 }
