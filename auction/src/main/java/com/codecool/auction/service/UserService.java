2b2df88501953f21a15ae6cf5ef86b1d5cbf4401
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-09-26 14:52:39 +0200

    compare hashed passwords in login, store hashed password in db

--- auction/src/main/java/com/codecool/auction/service/UserService.java
+++ auction/src/main/java/com/codecool/auction/service/UserService.java
@@ -1,29 +1,38 @@
 package com.codecool.auction.service;
-
 import com.codecool.auction.controller.dto.UserLoginDTO;
 import com.codecool.auction.controller.dto.UserSignUpDTO;
 import com.codecool.auction.model.User;
 import com.codecool.auction.repository.UserDAO;
+import com.codecool.auction.security.JwtService;
+import com.codecool.auction.security.user.Role;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserService {
     final UserDAO userDAO;
+    final PasswordEncoder passwordEncoder;
+    final JwtService jwtService;
 
     @Autowired
-    public UserService(UserDAO userDAO) {
+    public UserService(UserDAO userDAO, PasswordEncoder passwordEncoder, JwtService jwtService) {
         this.userDAO = userDAO;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
     public ResponseEntity<Integer> checkLoginUser (UserLoginDTO userLoginDTO) {
         User user = getUserByUsername(userLoginDTO.username());
-        if (user.getPassword().equals(userLoginDTO.password())){
+
+        boolean isPasswordValid = passwordEncoder.matches(userLoginDTO.password(), user.getPassword());
+        if (isPasswordValid){
+            String token = jwtService.buildToken(user.getUserName());
             return new ResponseEntity<>(200, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(401, HttpStatus.UNAUTHORIZED);
@@ -38,9 +47,11 @@ public class UserService {
         String username = userSignUpDTO.username();
         String firstName = userSignUpDTO.firstname();
         String lastName = userSignUpDTO.lastname();
-        String password = userSignUpDTO.password();
+        String password = passwordEncoder.encode(userSignUpDTO.password());
         String email = userSignUpDTO.email();
-        User user = new User(-1L, username, firstName, lastName, password, email, null, null);
+        User user = new User(-1L, username, firstName, lastName, password, email, Role.USER, null, null);
         userDAO.save(user);
     }
+
+
 }
