d32e4953d0ca64b382412d36bb490699e939c74f
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-09-26 14:54:08 +0200

    add authentication filter class

---
+++ auction/src/main/java/com/codecool/auction/security/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.codecool.auction.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException
+    {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        //String username = JwtService.extractUsername(jwt);
+    }
+}
