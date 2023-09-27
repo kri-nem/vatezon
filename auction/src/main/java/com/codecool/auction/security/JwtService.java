9e948086d786f01ecc5eb30ae5c9dd83d5e560dc
fulopco <you@example.com>
2023-09-27 14:59:00 +0200

    extract claims from token

--- auction/src/main/java/com/codecool/auction/security/JwtService.java
+++ auction/src/main/java/com/codecool/auction/security/JwtService.java
@@ -1,24 +1,42 @@
 package com.codecool.auction.security;
+
+import com.codecool.auction.security.user.Role;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    @Value("${application.security.jwt.secret_key}")
-    private String secretKey;
+    //@Value("${application.security.jwt.secret_key}")
+    private final String secretKey  = System.getenv("secret_key");
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 
-    public String buildToken (String username) {
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("roles", String.class);
+    }
+
+    public String buildToken (String username, Role role) {
         return Jwts
                 .builder()
                 .setSubject(username)
+                .claim("roles", "ROLE_"+role)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
