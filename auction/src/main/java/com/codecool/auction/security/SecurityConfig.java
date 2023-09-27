9d6ba74039f7f2fcbb4a8d83ad78d46bd92101a3
fulopco <you@example.com>
2023-09-27 14:57:04 +0200

    add filter to filterChain

--- auction/src/main/java/com/codecool/auction/security/SecurityConfig.java
+++ auction/src/main/java/com/codecool/auction/security/SecurityConfig.java
@@ -1,4 +1,5 @@
 package com.codecool.auction.security;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -6,10 +7,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
@@ -20,7 +25,8 @@ public class SecurityConfig {
                     req.anyRequest().authenticated();
                 })
 
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
