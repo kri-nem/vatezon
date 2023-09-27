f9d379fbae951804ed267f5afb5b4e68a855d8fd
fulopco <you@example.com>
2023-09-27 14:59:39 +0200

    implement authentication filter

--- auction/src/main/java/com/codecool/auction/security/JwtAuthenticationFilter.java
+++ auction/src/main/java/com/codecool/auction/security/JwtAuthenticationFilter.java
@@ -6,28 +6,47 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Set;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException
-    {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
+  private final JwtService jwtService;
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+  public JwtAuthenticationFilter(JwtService jwtService) {
+    this.jwtService = jwtService;
+  }
 
-        jwt = authHeader.substring(7);
-        //String username = JwtService.extractUsername(jwt);
+  @Override
+  protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                  @NonNull HttpServletResponse response,
+                                  @NonNull FilterChain filterChain) throws ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+    final String jwt;
+
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
     }
+    jwt = authHeader.substring(7);
+    String userName = jwtService.extractUsername(jwt);
+    String role = jwtService.extractRole(jwt);
+    if (userName != null) {
+      UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+              null,
+              null,
+              Set.of(new SimpleGrantedAuthority(role))
+      );
+      SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
+      filterChain.doFilter(request, response);
+  }
+
 }
