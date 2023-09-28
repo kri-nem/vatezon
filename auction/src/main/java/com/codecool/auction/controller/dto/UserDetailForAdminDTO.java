e26e4b5dcb76df7861f8e991813ac265dc2f6882
fulopco <you@example.com>
2023-09-28 11:27:40 +0200

    created userDTO for admin-role

--- auction/src/main/java/com/codecool/auction/controller/dto/UserDetailForAdminDTO.java
+++ auction/src/main/java/com/codecool/auction/controller/dto/UserDetailForAdminDTO.java
@@ -1,2 +1,3 @@
-package com.codecool.auction.controller.dto;public class UserDetailForAdminDTO {
-}
+package com.codecool.auction.controller.dto;
+
+public record UserDetailForAdminDTO (String userName, String firstName, String lastName, String email) {}
