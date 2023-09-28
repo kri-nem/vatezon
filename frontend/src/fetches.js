f29c49ff9f2a411ed237e93fab4253b2b423c788
Kristóf Németh <nemethkristof1@gmail.com>
2023-09-28 08:38:55 +0200

    make fetches reside in centralized location

---
+++ frontend/src/fetches.js
@@ -0,0 +1,35 @@
+const PRODUCTS_ROOT_URL = '/api/products'
+const PRODUCT_BY_CATEGORIES_URL = PRODUCTS_ROOT_URL + '/category/'
+const PRODUCTS_BY_NAME = PRODUCTS_ROOT_URL + '/name/'
+const PRODUCT_DETAILS_URL = PRODUCTS_ROOT_URL + '/detailed/'
+const NEW_PRODUCT_URL = PRODUCTS_ROOT_URL;
+const USERS_ROOT_URL = 'api/users'
+const SIGN_UP_URL = USERS_ROOT_URL + '/signup'
+const LOGIN_URL = USERS_ROOT_URL + '/login'
+
+const getDataWithAuthorization = (url) => fetch(url, {
+    headers: {
+        Authentication: 'Bearer ' + sessionStorage.getItem('token')
+    }
+})
+
+const postFormData = (url, formData) => fetch(url, {
+    method: 'POST',
+    body: formData
+})
+
+const postJson = (url, data) => fetch(url, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+})
+
+export const getProducts = () => getDataWithAuthorization(PRODUCTS_ROOT_URL)
+export const getProductsByCategory = (category) => getDataWithAuthorization(PRODUCT_BY_CATEGORIES_URL + category)
+export const getProductsByName = (name) => getDataWithAuthorization(PRODUCTS_BY_NAME + name)
+export const getProductDetails = (id) => getDataWithAuthorization(PRODUCT_DETAILS_URL + id)
+export const postNewProductForm = (formData) => postFormData(NEW_PRODUCT_URL, formData)
+export const postSignUp = (data) => postJson(SIGN_UP_URL, data)
+export const postLogIn = (data) => postJson(LOGIN_URL, data)
